39e49dc49ff360ed452740aa528866080b9c03d7
Origina-sudo <arthurba8585@gmail.com>
2024-10-02 13:25:13 +0000

    Creating views in the ask app

--- TaskManagementAPI/tasks_app/views.py
+++ TaskManagementAPI/tasks_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
