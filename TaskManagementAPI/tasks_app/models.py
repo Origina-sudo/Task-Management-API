2bc5b6e86b2891f5dfc7a4fac8acc0e8a7058697
Origina-sudo <arthurba8585@gmail.com>
2024-10-02 13:22:18 +0000

    Initializing models wihtin project

--- TaskManagementAPI/tasks_app/models.py
+++ TaskManagementAPI/tasks_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateTimeField()
+    priority_level = models.CharField(
+        max_length=10,
+        choices=[
+            ('Low', 'Low'),
+            ('Medium', 'Medium'),
+            ('High', 'High'),
+        ],
+        default='Medium'
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            ('Pending', 'Pending'),
+            ('Completed', 'Completed'),
+        ],
+        default='Pending'
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
